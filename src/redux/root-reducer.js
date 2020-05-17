8fa037f63fe0d985495f907031c1c3ef9c45dbd8
Faiz Hameed <faizftb@gmail.com>
2020-05-17 11:08:10 +0530

    Update root-reducer.js
    Fixed state changes to reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,15 +1,23 @@
-import { combineReducers } from "redux";
-
-const initialState = {
-  books: [],
+import { combineReducers } from  "redux";
+import { actionTypes } from  "./types";
+const  initialState  = {
+books: [],
 };
-
-function bookReducer(state = initialState, action) {
-  return state;
+function  bookReducer(state  =  initialState, action) {
+switch (action.type) {
+case  actionTypes.ADD_BOOK:
+return {
+	...state,
+	books: [...state.books,  action.payload],
+};
+default:
+	break;
+}
+	return  state;
 }
 
-const rootReducer = combineReducers({
-  bookReducer,
+const  rootReducer  =  combineReducers({
+	bookReducer,
 });
 
-export default rootReducer;
+export  default  rootReducer;
