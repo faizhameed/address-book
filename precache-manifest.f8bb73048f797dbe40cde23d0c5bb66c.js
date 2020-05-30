24647697e595cf81ee4d7a0f96986541b53b3ec4
Faiz Hameed <faizftb@gmail.com>
2020-05-30 19:55:12 +0530

    Updates

---
+++ precache-manifest.f8bb73048f797dbe40cde23d0c5bb66c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc55f647533cee404f3a1a154f31219",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "1b88263fcde3a62908a3",
+    "url": "/address-book/static/css/3.94d73b1f.chunk.css"
+  },
+  {
+    "revision": "d8b6cadab2656c6bdc21",
+    "url": "/address-book/static/css/5.3b2e14a1.chunk.css"
+  },
+  {
+    "revision": "56651f6b381e1b6bbfbe",
+    "url": "/address-book/static/css/6.d40502bc.chunk.css"
+  },
+  {
+    "revision": "db70be743fedf09fac88",
+    "url": "/address-book/static/css/7.2b475765.chunk.css"
+  },
+  {
+    "revision": "8c3d7c6346b7d8e8b2d4",
+    "url": "/address-book/static/css/main.a245f7f5.chunk.css"
+  },
+  {
+    "revision": "ab5dc34baab46b6143e4",
+    "url": "/address-book/static/js/0.58934413.chunk.js"
+  },
+  {
+    "revision": "efb458a97117f132bd28b58fc490db1f",
+    "url": "/address-book/static/js/0.58934413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b88263fcde3a62908a3",
+    "url": "/address-book/static/js/3.cfc13a26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/address-book/static/js/3.cfc13a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9743d1aa7dfff349911",
+    "url": "/address-book/static/js/4.295b50e3.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/address-book/static/js/4.295b50e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b6cadab2656c6bdc21",
+    "url": "/address-book/static/js/5.afe7b7d0.chunk.js"
+  },
+  {
+    "revision": "56651f6b381e1b6bbfbe",
+    "url": "/address-book/static/js/6.987a0af1.chunk.js"
+  },
+  {
+    "revision": "db70be743fedf09fac88",
+    "url": "/address-book/static/js/7.2e7a5c0b.chunk.js"
+  },
+  {
+    "revision": "229a8f181db3f2346508",
+    "url": "/address-book/static/js/8.41a02b22.chunk.js"
+  },
+  {
+    "revision": "8c3d7c6346b7d8e8b2d4",
+    "url": "/address-book/static/js/main.ae0b30c7.chunk.js"
+  },
+  {
+    "revision": "6ef366733169259ac2d6",
+    "url": "/address-book/static/js/runtime-main.745a6733.js"
+  },
+  {
+    "revision": "479ab7bb5cb217f5a0bdd60e374931f1",
+    "url": "/address-book/static/media/background.479ab7bb.jpeg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/address-book/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/address-book/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/address-book/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/address-book/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/address-book/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/address-book/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/address-book/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/address-book/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/address-book/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/address-book/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/address-book/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/address-book/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/address-book/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/address-book/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/address-book/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/address-book/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
