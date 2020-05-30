3dfad42360abf6172e91fba92fc0363efda2f2e1
Faiz Hameed <faizftb@gmail.com>
2020-05-30 21:23:58 +0530

    go back btn fix

--- src/pages/ContactDetails/ContactDetails.jsx
+++ src/pages/ContactDetails/ContactDetails.jsx
@@ -32,7 +32,9 @@ const ContactDetails = ({ person }) => {
          * Go back to address table
          */}
         <Link style={{ color: "#fff" }} to="/">
-          <Button primary>Go Back</Button>
+          <Button style={{ margin: "10px 0 20px" }} primary>
+            Go Back
+          </Button>
         </Link>
       </div>
     </React.Fragment>
